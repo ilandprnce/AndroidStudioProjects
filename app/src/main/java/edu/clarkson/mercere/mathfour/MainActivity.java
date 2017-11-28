5fbf6dbb949a9243b2a4ebec374ded0e508e8fe8
ilandprnce <30669416+ilandprnce@users.noreply.github.com>
2017-11-27 19:52:20 -0500

    #6 - A lot of stuff here... MathMaker is a class dedicated to making math problems, while ParserII is Daqing's parser from the Calculator project. In the main activity is the "broken" backbone to the answer selection system.

--- app/src/main/java/edu/clarkson/mercere/mathfour/MainActivity.java
+++ app/src/main/java/edu/clarkson/mercere/mathfour/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.clarkson.mercere.mathfour;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,8 +11,8 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private RadioGroup radioGroup;
-    private RadioButton bA,bB,bC,bD;
+
+    private Button bA,bB,bC,bD;
     private TextView questionBox, userPrompt;
     private MathMaker math;
 
@@ -27,11 +28,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //GUI Components-----------------------------------
-        radioGroup = (RadioGroup) findViewById(R.id.radio);
-        bA = (RadioButton) findViewById(R.id.A);
-        bB = (RadioButton) findViewById(R.id.B);
-        bC = (RadioButton) findViewById(R.id.C);
-        bD = (RadioButton) findViewById(R.id.D);
+
+        bA = (Button) findViewById(R.id.A);
+        bB = (Button) findViewById(R.id.B);
+        bC = (Button) findViewById(R.id.C);
+        bD = (Button) findViewById(R.id.D);
 
 
         questionBox = (TextView) findViewById(R.id.QuestionBox);
@@ -44,47 +45,75 @@ public class MainActivity extends AppCompatActivity {
         //-------------------------------------------------
 
 
-
         // Everytime a Question is made and Answered
 
-            //Shown first this when a question is prompted
+        //Shown first this when a question is prompted
 
 
-        equation=math.eqn;// makes an equation "x ? y"
+        equation = math.eqn;// makes an equation "x ? y"
         options = math.options; // makes a 4 long array that is filled with numbers + or minus 2 of the answer as well as the answer. {1,2,X,4} X = 3 is the answer
 
         userPrompt.setText("Solve");
         questionBox.setText(equation);
+        updateButtons();
+        /*
         bA.setText(Integer.toString(math.options[0]));
         bB.setText(Integer.toString(math.options[1]));
         bC.setText(Integer.toString(math.options[2]));
         bD.setText(Integer.toString(math.options[3]));
+*/
+
+        View.OnClickListener Selection = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button b = (Button) v ;
 
 
-        public void onRadioButtonClicked(View v){
-
-
-        Log.v("od_ButtonPress",b.getText()+"pressed");
 
                 correctAnswer = false;
 
-                if(b.getText().equals(Integer.toString(math.ans))){
-                    userPrompt.setText("Correct! The Answer was "+ math.ans);
-                    Log.v("od_Correct","Right");
+                if (b.getText().equals(Integer.toString(math.ans))) {
+                    userPrompt.setText("Correct! The Answer was " + math.ans);
+                    questionBox.setText("");
+                    Log.v("od_Correct", "Right");
 
 
                     correctAnswer = true;
-                }else{
-                    userPrompt.setText("Incorrect. The Answer was "+ math.ans);
-                    Log.v("od_Wrong","Wrong");
+                } else {
+                    userPrompt.setText("Incorrect. The Answer was " + math.ans);
+                    Log.v("od_Wrong", "Wrong");
                 }
             }
         };
 
+        bA.setOnClickListener(Selection);
+        bB.setOnClickListener(Selection);
+        bC.setOnClickListener(Selection);
+        bD.setOnClickListener(Selection);
+
+
+        if(correctAnswer == true){
+            // do a thing?    Go back to the other Activity... RESET?!
+           reset();
+
+        }
+
+    } // put listener after this
+
+    public void reset(){
+        math.genEqn();
+        
+        math.makeOptions();
+        updateButtons();
+    }
+
+
+    public void updateButtons(){
+        bA.setText(Integer.toString(math.options[0]));
+        bB.setText(Integer.toString(math.options[1]));
+        bC.setText(Integer.toString(math.options[2]));
+        bD.setText(Integer.toString(math.options[3]));
+    }
     } // end of onCreate()
 
 
-
-
-
-}
